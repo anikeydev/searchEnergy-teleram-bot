f1be111ec8036ba9c184db1c663d353eef91dd69
anikey vladislav <dev.anikey.va@mail.ru>
2025-03-14 18:00:49 +0300

    dev

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ config()
 const app = express()
 const PORT = process.env.PORT
 
+
+
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
     startBot()
--- utils.js
+++ utils.js
@@ -1,4 +1,4 @@
-function compairCoordinates (coord1, coord2, radius = 0.03) {
+function compairCoordinates (coord1, coord2, radius = 0.025) {
     if (coord1[0] === coord2[0] && coord1[1] === coord2[1]) {
         return true
     } else if (coord1[0] >= coord2[0] && coord1[0] <= coord2[0]+radius && coord1[1] >= coord2[1] && coord1[1] <= coord2[1]+radius) {
